61b1fa72bc726b2a8b1c67c809c620b96abab727
henhal <henhalvor@gmail.com>
2023-08-26 20:27:14 +0200

    add logic to app.js

---
+++ app.js
@@ -0,0 +1,24 @@
+import express from "express";
+import { runRealEstateScraper } from "./realestateScraper.js";
+
+const app = express();
+
+app.get("/", async (req, res) => {
+    const url = req.query.url;
+
+    if (!url) {
+        return res.status(400).json({ error: 'URL parameter is required.' });
+    }
+
+    try {
+        const data = await runRealEstateScraper(url);
+        return res.json(data);
+    } catch (error) {
+        return res.status(500).json({ error: 'An error occurred while scraping the data.' });
+    }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
